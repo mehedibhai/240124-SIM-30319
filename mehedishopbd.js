4af186a5750c91906c1ec533d3413efb55a8d0e8
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-05-23 17:30:42 +0100

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.mehedibhai.shop/?utm_medium=493997d8325eb1ff9c779d76b1573880814e592e&utm_campaign=newgp1";
-var bllink = "https://aff.mehedibhai.shop/?utm_medium=294d28833dc72944f6977ebb0e2a1bf4affcd0c3&utm_campaign=newbl1";
+var gplink = "https://qsvdhat.com/cl/41e930d4714a79aa?p1=&p2=&source=&site=VALOLAGENAHGP1";
+var bllink = "https://qsvdhat.com/cl/e49844e89e57cb0b?p1=&p2=&source=&site=VALOLAGENAGVBL1";
 var arlink = "https://aff.mehedibhai.shop/?utm_medium=8a230149472bf299e901e110252fc19dcbe6b4bf&utm_campaign=newrobi1";
-var rblink = "https://aff.mehedibhai.shop/?utm_medium=8a230149472bf299e901e110252fc19dcbe6b4bf&utm_campaign=newrobi1";
+var rblink = "https://qsvdhat.com/cl/805afe4ee39bd725?p1=&p2=&source=&site=VALOLAGENAROBI1";
