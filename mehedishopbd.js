f31839957ec35aa37640a1638228cb20a290aee9
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-08-12 10:03:38 +0100

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.mehedibhai.shop/?utm_medium=0954f5da13be51497b2456b9e08f80e4b5ccb59e&utm_campaign=mayagp111";
 var bllink = "https://aff.mehedibhai.shop/?utm_medium=7fcc81837bf64f1b16aad6d66f966a4a0a18aa98&utm_campaign=mayabl1";
 var arlink = "https://aff.mehedibhai.shop/?utm_medium=e90ee668a7b64e432d51974a079832686b72da9c&utm_campaign=mayaartel1";
-var rblink = "https://aff.mehedibhai.shop/?utm_medium=560d22464f48565c69f3c497df79273e56521631&utm_campaign=mayagp12";
+var rblink = "https://aff.mehedibhai.shop/?utm_medium=b7913ca6160e2254519c04f36cc773f2b61e9883&utm_campaign=mayarobi111";
