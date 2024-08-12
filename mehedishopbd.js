d79efab9cad4320b5ec663c0c6ed0aca746c8f1e
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-08-12 10:04:23 +0100

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.mehedibhai.shop/?utm_medium=0954f5da13be51497b2456b9e08f80e4b5ccb59e&utm_campaign=mayagp111";
+var gplink = "https://aff.mehedibhai.shop/?utm_medium=86123a84e1829a2cc65e9171aaf31ed98c1c2e73&utm_campaign=maya222gp";
 var bllink = "https://aff.mehedibhai.shop/?utm_medium=7fcc81837bf64f1b16aad6d66f966a4a0a18aa98&utm_campaign=mayabl1";
 var arlink = "https://aff.mehedibhai.shop/?utm_medium=e90ee668a7b64e432d51974a079832686b72da9c&utm_campaign=mayaartel1";
 var rblink = "https://aff.mehedibhai.shop/?utm_medium=b7913ca6160e2254519c04f36cc773f2b61e9883&utm_campaign=mayarobi111";
