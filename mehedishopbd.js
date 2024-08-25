e0383568c427ceea5384875882f252c42fbc4300
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-08-26 00:33:20 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://ciqspfe.tihvpvp.com/cl/bbad312445a796e4?p1=&p2=&source=momin&site=gpbl";
-var bllink = "https://ciqspfe.tihvpvp.com/cl/bbad312445a796e4?p1=&p2=&source=momin&site=gpbl";
+var gplink = "https://aff.mehedibhai.shop/?utm_medium=63babbee8a2e276936cb04d845466b159caf6c32&utm_campaign=kholilgp";
+var bllink = "https://aff.mehedibhai.shop/?utm_medium=89ed3b3c7c2c01463a224670e8a126b28785d582&utm_campaign=kholilbl";
 var arlink = "https://aff.mehedibhai.shop/?utm_medium=51e24fd4200bfef518b0e8c88d63d7b157bc61a6&utm_campaign=khoililartel5";
 var rblink = "https://aff.mehedibhai.shop/?utm_medium=49173c91234ec7b3d25de7fb74c68d3df8bb9712&utm_campaign=newrobinnn";
