c8fa6ce3abe2d3ffe8a0f1328be7d7e1abe35a21
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-08-26 19:49:13 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.mehedibhai.shop/?utm_medium=76b1c27eb7a772aece839b81eb176c22a4b5f7e2&utm_campaign=dhakagp";
+var gplink = "https://aff.mehedibhai.shop/?utm_medium=3466ded80944d277234f10c9727624a011909762&utm_campaign=Dhaka1gp";
 var bllink = "https://aff.mehedibhai.shop/?utm_medium=91e139cd1b5ec82659299daa386b5c966611b564&utm_campaign=dhakabl";
-var arlink = "https://aff.mehedibhai.shop/?utm_medium=024abc81163e1d8d8bd435cb3228c2389db49a18&utm_campaign=dhakarobiartel";
-var rblink = "https://aff.mehedibhai.shop/?utm_medium=024abc81163e1d8d8bd435cb3228c2389db49a18&utm_campaign=dhakarobiartel";
+var arlink = "https://aff.mehedibhai.shop/?utm_medium=a6a199d90535115ea175a3a5c8f8c627acc73e5e&utm_campaign=Artelrobi";
+var rblink = "https://aff.mehedibhai.shop/?utm_medium=a6a199d90535115ea175a3a5c8f8c627acc73e5e&utm_campaign=Artelrobi";
