5e7f281dedf57af4464bd61cfdca46f34831ffda
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-08-27 16:09:07 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.mehedibhai.shop/?utm_medium=3466ded80944d277234f10c9727624a011909762&utm_campaign=Dhaka1gp";
-var bllink = "https://aff.mehedibhai.shop/?utm_medium=91e139cd1b5ec82659299daa386b5c966611b564&utm_campaign=dhakabl";
+var gplink = "https://difzjve.tihvpvp.com/cl/56c9dabce7bdf80d?p1=&p2=&source=&site=Mongp";
+var bllink = "https://difzjve.tihvpvp.com/cl/642406fc200759d1?p1=&p2=&source=&site=Monbl";
 var arlink = "https://aff.mehedibhai.shop/?utm_medium=d0cd0817abe6b7f1bf7da94cd7eda10fe69787b3&utm_campaign=Dhakarobi";
-var rblink = "https://aff.mehedibhai.shop/?utm_medium=d0cd0817abe6b7f1bf7da94cd7eda10fe69787b3&utm_campaign=Dhakarobi";
+var rblink = "https://difzjve.tihvpvp.com/cl/735a2b574bbbc0af?p1=&p2=&source=&site=Monrobi";
