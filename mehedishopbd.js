875b02f8f0c39f39f1546bae3bb4899b60e1e77a
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-08-29 10:45:11 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.mehedibhai.shop/?utm_medium=8685dcd478b113c17196c4ee9556e0d7b432e6c6&utm_campaign=Silpigp";
-var bllink = "https://aff.mehedibhai.shop/?utm_medium=787440b6aa6ac4a959e9ff7aefca24069e242e29&utm_campaign=Silpibl";
+var gplink = "https://aff.mehedibhai.shop/?utm_medium=aeb51b2910e00b3d86d46206b283996a8bcb8926&utm_campaign=Mayagp";
+var bllink = "https://aff.mehedibhai.shop/?utm_medium=ab9d5b4d34018883bf48089858135b025aab29c0&utm_campaign=Mayabl";
 var arlink = "https://aff.mehedibhai.shop/?utm_medium=57d0f1043520efc3b5a56e0480e7257cb4199b71&utm_campaign=Silpirobiartel";
 var rblink = "https://aff.mehedibhai.shop/?utm_medium=57d0f1043520efc3b5a56e0480e7257cb4199b71&utm_campaign=Silpirobiartel";
