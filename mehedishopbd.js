8bb6698cd1a03e6fa8e6d2b2007d3d42824232fa
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-09-05 15:54:59 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
 var gplink = "https://msm.mahedims.xyz/?utm_medium=9fa8925ceedd98783bf42469f64c792b522f85b7&utm_campaign=Gp";
-var bllink = "https://msm.mahedims.xyz/?utm_medium=a3ce0250ce3c8afd25f45a74eb1b74116bf33a88&utm_campaign=Mitabl";
+var bllink = "https://msm.mahedims.xyz/?utm_medium=ab3168199426121e3e746a5bd5a707b4b9331711&utm_campaign=Bl";
 var arlink = "https://msm.mahedims.xyz/?utm_medium=55d86c1d7b046aad833a1b323194bfc2df4f076b&utm_campaign=msartelrobi";
 var rblink = "https://msm.mahedims.xyz/?utm_medium=55d86c1d7b046aad833a1b323194bfc2df4f076b&utm_campaign=msartelrobi";
