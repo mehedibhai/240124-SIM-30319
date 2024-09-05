2394fb45626ccac286a309a86ddef602e06e49e2
mehedibhai <157568444+mehedibhai@users.noreply.github.com>
2024-09-06 00:17:38 +0600

    Update mehedishopbd.js

--- mehedishopbd.js
+++ mehedishopbd.js
@@ -1,4 +1,4 @@
-var gplink = "https://msm.mahedims.xyz/?utm_medium=9fa8925ceedd98783bf42469f64c792b522f85b7&utm_campaign=Gp";
-var bllink = "https://msm.mahedims.xyz/?utm_medium=ab3168199426121e3e746a5bd5a707b4b9331711&utm_campaign=Bl";
+var gplink = "https://svlumrx.com/cl/dfbd277a2776d5d4?p1=&p2=&source=gp&site=sim";
+var bllink = "https://svlumrx.com/cl/a0ca37d42015486a?p1=&p2=&source=bl&site=sim";
 var arlink = "https://msm.mahedims.xyz/?utm_medium=55d86c1d7b046aad833a1b323194bfc2df4f076b&utm_campaign=msartelrobi";
 var rblink = "https://msm.mahedims.xyz/?utm_medium=55d86c1d7b046aad833a1b323194bfc2df4f076b&utm_campaign=msartelrobi";
